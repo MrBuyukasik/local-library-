64b0071d020719681ec3c991a0de4a34ab16c413
Doğan Akdeniz <dogan.akdeniz@ogr.deu.edu.tr>
2017-05-06 15:39:14 +0300

    Kategori ekledim

---
+++ Library/Library/Library/Kategori.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Kategori
+    {
+        ArrayList kategoriAdi = new ArrayList();
+        public Kategori()
+        {
+            SetKategoriAdi();
+        }
+        void SetKategoriAdi()
+        {
+            string path = @"..\DB\Library\";
+            string[] dir = Directory.GetDirectories(path);
+            foreach (string item in dir)
+            {
+                kategoriAdi.Add(item.Substring(item.LastIndexOf('\\') + 1));
+            }
+        }
+    }
+}
