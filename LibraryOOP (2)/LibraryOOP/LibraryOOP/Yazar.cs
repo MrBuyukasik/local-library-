d521c53822dfd84eeeadceeefc23ffa0091dd7c0
Hasan Can Büyükaşık <hasan.1_@hotmail.com>
2017-05-13 15:49:12 +0300

    New Version

---
+++ LibraryOOP (2)/LibraryOOP/LibraryOOP/Yazar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryOOP
+{
+    class Yazar : Kategori
+    {
+        public List<string> yazar = new List<string>();
+        public void SetYazar(string adi)
+        {
+            if (!yazar.Contains(adi)) yazar.Add(adi);
+        }
+    }
+}
